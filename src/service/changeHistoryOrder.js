733ed31ea5bc645b7a31e67bbaacc65c7dfc4fa8
avitalg6297 <137280930+avitalg6297@users.noreply.github.com>
2023-08-07 16:08:12 +0300

    Update changeHistoryOrder.js
    changeHistoryOrder comments of dvir

--- src/service/changeHistoryOrder.js
+++ src/service/changeHistoryOrder.js
@@ -1,4 +1,5 @@
 function changeHistoryOrder(historyDirection, setHistoryDirection) {
+    //what is 1 and 0 u use to set with it ?
     if (historyDirection === 0) {
       setHistoryDirection(1);
     } else {
