b5bc6a6ed7dabfc176cb0bf4287e37e9fac471e6
avitalg6297 <137280930+avitalg6297@users.noreply.github.com>
2023-08-07 16:05:55 +0300

    Update createSquare.jsx
    CreateSquare comments of dvir

--- src/components/squareComponent/createSquare.jsx
+++ src/components/squareComponent/createSquare.jsx
@@ -3,7 +3,9 @@ import Square from "./square";
 import { useHandleClick } from "../../service/useHandleClick";
 
 function CreateSquare(squareNumber,isWinningSquare,squares,squaresByIndexes, xIsNext) {
-
+   // alt shift f 
+   // use is a save word for hooks, its great to create a costum hooks but not in services 
+   // CreateSquare is basiclly a warpper to Square componenet as i understood , is it required ? 
    let {handleClick} = useHandleClick();
 
   return (
